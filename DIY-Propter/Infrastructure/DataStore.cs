2ecbdbe3b5e5c9eead30e57e55651c7e4fb4a590
Markus Keil <markus@keil-connect.com>
2015-01-24 10:55:55 +0100

    added mongodb stuff

---
+++ DIY-Propter/Infrastructure/DataStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Configuration;
+using System.Web.Hosting;
+
+namespace DIY_Propter.Infrastructure
+{
+    public class DataStore
+    {
+        public static MongoCollection<T> GetCollection<T>(string collection)
+        {
+            var mclient = new MongoClient(ConfigurationManager.ConnectionStrings["mongo"].ConnectionString);
+            MongoServer srv = mclient.GetServer();
+            var db = srv.GetDatabase("diypromtper");
+            return db.GetCollection<T>(collection);
+        }
+    }
+}
