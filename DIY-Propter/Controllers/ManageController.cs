8931a80dc237db8c1ca28750159859d397f81aaf
Markus Keil <markus@keil-connect.com>
2015-01-24 12:55:14 +0100

    added basic scraffold for list/create

--- DIY-Propter/Controllers/ManageController.cs
+++ DIY-Propter/Controllers/ManageController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DIY_Propter.Infrastructure;
+using DIY_Propter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,15 +13,98 @@ namespace DIY_Propter.Controllers
         //
         // GET: /Manage/
 
-        public ActionResult List()
+        public ActionResult Index()
+        {
+            List<Prompting> prompts = DataStore.GetCollection<Prompting>("promptings").FindAll().ToList();
+            return View(prompts);
+        }
+
+        //
+        // GET: /Manage/Details/5
+
+        public ActionResult Details(int id)
         {
             return View();
         }
+
+        //
+        // GET: /Manage/Create
 
         public ActionResult Create()
         {
+            Prompting p = new Prompting();
+            return View(p);
+        }
+
+        //
+        // POST: /Manage/Create
+
+        [HttpPost]
+        public ActionResult Create(Prompting p)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var prompts = DataStore.GetCollection<Prompting>("promptings");
+                prompts.Save(p);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Manage/Edit/5
+
+        public ActionResult Edit(int id)
+        {
             return View();
         }
 
+        //
+        // POST: /Manage/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Manage/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Manage/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
---
+++ DIY-Propter/Models/Prompting.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DIY_Propter.Models
+{
+    public class Prompting
+    {
+        [BsonId]
+        public Guid PromptID { get; set; }
+
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+    }
+}
